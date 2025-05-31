f7f202e63e0cf72e0bcf8ada5f67144e924e7ee7
LuanaKKemn <luanakkemndtooa@gmail.com>
2025-05-30 22:37:48 -0300

    Nível 3

---
+++ INICIANTE/1.024.py
@@ -0,0 +1,13 @@
+
+n = int(input())
+
+for i in range(n):
+    entrada = list(input())
+
+    ## Criptografar com 3 funções
+
+
+
+    ## 3 posições pra frente -  direita
+    ## inverter tudo
+    ## segunda metade desloca uma letra pra esquerda do alfabeto
