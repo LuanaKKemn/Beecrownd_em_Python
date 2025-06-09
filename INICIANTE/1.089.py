13dc14ca2add5d039f41a0c1648359075f81a7b1
LuanaKKemn <luanakkemndtooa@gmail.com>
2025-06-08 23:52:07 -0300

    Nível 9

---
+++ INICIANTE/1.089.py
@@ -0,0 +1,19 @@
+
+while True:
+    n = int(input())
+
+    if n == 0:
+        break
+
+    hi = list(map(int, input().split()))
+
+    loop = []
+    for i in range(1, n+1):
+
+
+
+    print(len((loop)))
+
+
+
+
